a238709179fed8a3510d020bac516b9f6499f984
Phil Wang <lucidrains@gmail.com>
2020-11-04 17:24:57 -0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '0.0.14',
+  version = '0.0.15',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
