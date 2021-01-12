b82afd71ade8dc4e823b62bae6da0184be144ae4
Phil Wang <lucidrains@gmail.com>
2021-01-11 16:07:25 -0800

    allow floats for ff_mult

--- x_transformers/x_transformers.py
+++ x_transformers/x_transformers.py
@@ -197,16 +197,17 @@ class GEGLU(nn.Module):
 class FeedForward(nn.Module):
     def __init__(self, dim, dim_out = None, mult = 4, glu = False, dropout = 0.):
         super().__init__()
+        inner_dim = int(dim * mult)
         dim_out = default(dim_out, dim)
         project_in = nn.Sequential(
-            nn.Linear(dim, dim * mult),
+            nn.Linear(dim, inner_dim),
             nn.GELU()
-        ) if not glu else GEGLU(dim, dim * mult)
+        ) if not glu else GEGLU(dim, inner_dim)
 
         self.net = nn.Sequential(
             project_in,
             nn.Dropout(dropout),
-            nn.Linear(dim * mult, dim_out)
+            nn.Linear(inner_dim, dim_out)
         )
 
     def forward(self, x):
