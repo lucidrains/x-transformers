6b93c21be0d0a679da6f7b9621d9bb638ab18428
Phil Wang <lucidrains@gmail.com>
2021-02-07 20:33:39 -0800

    add an assert for relative positional keyword arguments

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '0.8.1',
+  version = '0.8.3',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
