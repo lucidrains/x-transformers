9001922ca4d1daac8139330e2efefa1d41001a17
Had <had-95@yandex.ru>
2021-06-28 17:17:33 +0300

    Add Attention class for import

--- x_transformers/__init__.py
+++ x_transformers/__init__.py
@@ -1,2 +1,2 @@
-from x_transformers.x_transformers import XTransformer, Encoder, Decoder, CrossAttender, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
+from x_transformers.x_transformers import XTransformer, Encoder, Decoder, CrossAttender, Attention, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
 from x_transformers.autoregressive_wrapper import AutoregressiveWrapper
