0dc6942e982132c22328a06d1169db0b1431b587
Phil Wang <lucidrains@gmail.com>
2021-08-28 07:17:14 -0700

    do not persist alibi bias and slopes to state dict

--- x_transformers/x_transformers.py
+++ x_transformers/x_transformers.py
@@ -178,8 +178,8 @@ class AlibiPositionalBias(nn.Module):
         self.heads = heads
         slopes = torch.Tensor(self._get_slopes(heads))
         slopes = rearrange(slopes, 'h -> () h () ()')
-        self.register_buffer('slopes', slopes)
-        self.register_buffer('bias', None)
+        self.register_buffer('slopes', slopes, persistent = False)
+        self.register_buffer('bias', None, persistent = False)
 
     @staticmethod
     def _get_slopes(heads):
