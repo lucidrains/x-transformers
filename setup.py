b5767d9589a9d53876d7f3885488ab8e28a33188
Phil Wang <lucidrains@gmail.com>
2022-05-18 12:09:26 -0700

    0.28.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '0.28.2',
+  version = '0.28.3',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
