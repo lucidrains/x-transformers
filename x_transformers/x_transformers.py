655ff307094d8f1cbd02b27ab7e094b937309b01
Phil Wang <lucidrains@gmail.com>
2022-07-28 10:29:06 -0700

    more informative error message

--- x_transformers/x_transformers.py
+++ x_transformers/x_transformers.py
@@ -126,12 +126,16 @@ class AbsolutePositionalEmbedding(nn.Module):
     def __init__(self, dim, max_seq_len, l2norm_embed = False):
         super().__init__()
         self.scale = dim ** -0.5 if not l2norm_embed else 1.
+        self.max_seq_len = max_seq_len
         self.l2norm_embed = l2norm_embed
         self.emb = nn.Embedding(max_seq_len, dim)
 
     def forward(self, x, pos = None):
+        seq_len = x.shape[1]
+        assert seq_len <= self.max_seq_len, f'you are passing in a sequence length of {seq_len} but your absolute positional embedding has a max sequence length of {self.max_seq_len}'
+
         if not exists(pos):
-            pos = torch.arange(x.shape[1], device = x.device)
+            pos = torch.arange(seq_len, device = x.device)
 
         pos_emb = self.emb(pos)
         pos_emb = pos_emb * self.scale
