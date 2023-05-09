80bd4d946cf6e7253620df423ceb63f6f9534692
Phil Wang <lucidrains@gmail.com>
2023-05-09 09:40:47 -0700

    add ResiDual paper

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '1.12.3',
+  version = '1.14.0',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
