2a5bbdd83b74b61c944093b5d238b70aba196303
Phil Wang <lucidrains@gmail.com>
2023-06-30 06:41:46 -0700

    address masking logic for flash attention

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '1.16.11',
+  version = '1.16.12',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
