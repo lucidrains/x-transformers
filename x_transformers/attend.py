2d1aca2df5c282170b168cf5e5ab04cda18df077
lucidrains <lucidrains@gmail.com>
2023-09-21 11:18:11 -0700

    support https://github.com/lucidrains/x-transformers/issues/186

--- x_transformers/attend.py
+++ x_transformers/attend.py
@@ -125,8 +125,13 @@ class Attend(nn.Module):
 
         device_properties = torch.cuda.get_device_properties(torch.device('cuda'))
 
-        if device_properties.major == 8 and device_properties.minor == 0:
+        major, minor = device_properties.major, device_properties.minor
+
+        if (major, minor) == (8, 0):
             print_once('A100 GPU detected, using flash attention if input tensor is on cuda')
+            self.cuda_config = EfficientAttentionConfig(True, False, False)
+        elif (major, minor) == (9, 0):
+            print_once('H100 GPU detected, using flash attention')
             self.cuda_config = EfficientAttentionConfig(True, False, False)
         else:
             print_once('Non-A100 GPU detected, using math or mem efficient attention if input tensor is on cuda')
