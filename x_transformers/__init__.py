3698b74d1e3a98dcefebf5bf52400afdee768738
lucidrains <lucidrains@gmail.com>
2023-10-18 17:00:12 -0700

    add xval wrapper and autoregressive wrapper

--- x_transformers/__init__.py
+++ x_transformers/__init__.py
@@ -11,9 +11,14 @@ from x_transformers.x_transformers import (
 from x_transformers.autoregressive_wrapper import AutoregressiveWrapper
 from x_transformers.nonautoregressive_wrapper import NonAutoregressiveWrapper
 
-from x_transformers.continuous_autoregressive_wrapper import (
+from x_transformers.continuous import (
     ContinuousTransformerWrapper,
     ContinuousAutoregressiveWrapper
+)
+
+from x_transformers.xval import (
+    XValTransformerWrapper,
+    XValAutoregressiveWrapper
 )
 
 from x_transformers.xl_autoregressive_wrapper import XLAutoregressiveWrapper
