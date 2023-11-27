8be1a6ebc63bbeb45f54cd5cd2eb8912aabb780b
lucidrains <lucidrains@gmail.com>
2023-11-26 18:54:59 -0800

    allow continuous transformer wrapper to have prepend embedding mask too

--- x_transformers/continuous.py
+++ x_transformers/continuous.py
@@ -85,9 +85,10 @@ class ContinuousTransformerWrapper(nn.Module):
         mems = None,
         pos = None,
         prepend_embeds = None,
+        prepend_mask = None,
         **kwargs
     ):
-        batch = x.shape[0]
+        batch, seq, device = *x.shape[:2], x.device
 
         x = self.project_in(x)
         x = x + self.pos_emb(x, pos = pos)
@@ -107,10 +108,16 @@ class ContinuousTransformerWrapper(nn.Module):
         # whether to append embeds, as in PaLI, for image embeddings
 
         if exists(prepend_embeds):
-            _, prepend_dim = prepend_embeds.shape[1:]
+            prepend_seq, prepend_dim = prepend_embeds.shape[1:]
             assert prepend_dim == x.shape[-1], 'prepended embeddings need to have same dimensions as model dimensions'
 
             x = torch.cat((prepend_embeds, x), dim = -2)
+
+            if exists(prepend_mask) or exists(mask):
+                mask = default(mask, lambda: torch.ones((batch, seq), device = device, dtype = torch.bool))
+                prepend_mask = default(prepend_mask, lambda: torch.ones((batch, prepend_seq), device = device, dtype = torch.bool))
+
+                mask = torch.cat((prepend_mask, mask), dim = -1)
 
         x = self.emb_dropout(x)
 
