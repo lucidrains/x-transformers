3ebaa3f9f8b6c1d343334a8653d0929fe073735b
lucidrains <lucidrains@gmail.com>
2023-12-01 20:21:49 -0800

    support any number of additional embeddings, addressing https://github.com/lucidrains/x-transformers/issues/213

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '1.26.0',
+  version = '1.26.2',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
