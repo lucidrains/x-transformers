93a64785151fa78a3b58fc1e07e31186dc85f437
Ryan Kim <ryankim17920@gmail.com>
2024-03-18 22:10:17 -0500

    xl_multiIO basics done
     - memory system does not work and need fixes, basic training can function though (without memory)

--- IOtest.py
+++ IOtest.py
@@ -2,6 +2,7 @@ from x_transformers.x_transformers import AttentionLayers
 
 from x_transformers.multi_IO.IO_wrapper import MultiIOTransformerWrapper
 from x_transformers.multi_IO.autoregressive_multiO import MultiOAutoregressiveWrapper
+from x_transformers.multi_IO.xl_autoregressive_wrapper_multiO import MultiOXLAutoregressiveWrapper
 from x_transformers import Decoder, AutoregressiveWrapper, TransformerWrapper
 import torch
 
@@ -26,9 +27,9 @@ for i in range(10000):
 print(sum(p.numel() for p in model.parameters()))
 print(sum(p.numel() for p in model.parameters() if p.requires_grad))
 """
-"""
+
 # multi input to multi output
-model = MultiOAutoregressiveWrapper(
+model = MultiOXLAutoregressiveWrapper(
     outputs=2,
     #add_attn_z_loss=True,
     pad_value=torch.Tensor([0, 0, 0]),
@@ -66,7 +67,8 @@ print(sum(p.numel() for p in model.parameters() if p.requires_grad))
 # x = torch.Tensor(torch.randint(1, 3, (1, 10, 2))).float()
 # print(x)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-x = torch.Tensor([[[0, 1, 2], [0, 1, 2]]]).float()
+x = torch.Tensor([[[0, 1, 2], [0, 2, 2]]]).long()
+#print(x.shape)
 for i in range(5000):
     loss = model(x)
     optimizer.zero_grad()
@@ -74,7 +76,7 @@ for i in range(5000):
     optimizer.step()
     print(loss)
 print(model(x, return_outputs=True)[1][0])
-"""
+
 # 1.4163
 # 1.4083
 # print(model.generate(prompts=torch.Tensor([[[0, 1]]]).float(), seq_len=1))
@@ -82,7 +84,7 @@ print(model(x, return_outputs=True)[1][0])
 # 9336
 # for i in range(100):
 
-
+"""
 model = MultiIOTransformerWrapper(
     num_tokens=8,
     max_seq_len=10,
@@ -146,4 +148,4 @@ model = MultiIOTransformerWrapper(
 )
 x = torch.Tensor(torch.randint(1, 3, (1, 10, 3))).float()
 print(x)
-print(model(x))
+print(model(x))"""
---
+++ x_transformers/multi_IO/xl_autoregressive_wrapper_multiO.py
@@ -0,0 +1,157 @@
+from x_transformers.multi_IO import MultiIOTransformerWrapper
+from x_transformers.xl_autoregressive_wrapper import *
+
+
+class MultiOXLAutoregressiveWrapper(nn.Module):
+    def __init__(
+            self,
+            net,
+            outputs: int,
+            ignore_index=-100,
+            pad_value=0
+    ):
+        super().__init__()
+        self.pad_value = pad_value
+        self.ignore_index = ignore_index
+
+        self.outputs = outputs
+        self.net = net
+        if type(net) == MultiIOTransformerWrapper:
+            net.autoregressive = True
+        self.max_seq_len = net.max_seq_len
+
+    @torch.no_grad()
+    @eval_decorator
+    def generate(
+            self,
+            start_tokens,
+            seq_len,
+            eos_token=None,
+            temperature=1.,
+            filter_logits_fn=top_k,
+            filter_thres=0.9,
+            mems=None,
+            **kwargs
+    ):
+        device, max_seq_len = start_tokens.device, self.max_seq_len
+
+        start_tokens, ps = pack([start_tokens], '* n')
+
+        b, t = start_tokens.shape
+
+        *all_leading_tokens, _ = start_tokens.split(max_seq_len, dim=-1)
+
+        # catch the memory up to the current segment
+
+        for leading_tokens in all_leading_tokens:
+            _, mems = self.net(
+                leading_tokens,
+                mems=mems,
+                return_mems=True,
+                **kwargs
+            )
+
+        # now start sampling from the current segment
+
+        curr_pos = len(all_leading_tokens) * max_seq_len
+        curr_mems = mems
+
+        cache = None
+        out = start_tokens
+
+        for _ in range(seq_len):
+            curr_segment_len = out.shape[-1]
+            is_last_segment_tokens = divisible_by(curr_segment_len, max_seq_len)
+
+            x = out[:, curr_pos:]
+
+            logits_ = self.net(
+                x,
+                mems=curr_mems,
+                cache=cache,
+                return_mems=True,
+                return_intermediates=True,
+                **kwargs
+            )
+            logits = logits_[0]
+            cache = logits_[1]
+
+            sample = torch.Tensor([])
+            for i in range(self.outputs):
+                logits = logits[i][:, -1]
+                filtered_logits_i = filter_logits_fn(logits, thres=filter_thres)
+                probs_i = F.softmax(filtered_logits_i / temperature, dim=-1)
+
+                sample_i = torch.multinomial(probs_i, 1)
+
+                sample = torch.cat((sample, sample_i), dim=-1)
+                out = torch.cat((out, sample), dim=-1)
+                if is_last_segment_tokens:
+                    curr_pos = curr_segment_len
+                    curr_mems = cache
+
+                if exists(eos_token):
+                    is_eos_tokens = torch.all(torch.eq(out[:, :, :], eos_token), dim=-1)
+                    if torch.any(is_eos_tokens, dim=-1):
+                        break
+
+            if exists(eos_token):
+                # mask out everything after the eos tokens
+                shifted_is_eos_tokens = F.pad(is_eos_tokens, (1, -1))
+                mask = shifted_is_eos_tokens.float().cumsum(dim=-1) >= 1
+                out = torch.where(mask.unsqueeze(-1), self.pad_value, out)
+
+        out = out[:, t:]
+
+        out, = unpack(out, ps, '* n')
+
+        return out
+
+    def forward(
+            self,
+            x,
+            mems=None,
+            **kwargs
+    ):
+        ignore_index, max_seq_len = self.ignore_index, self.max_seq_len
+
+        x, labels = x[:, :-1], x[:, 1:]
+        seq_len = x.shape[1]
+
+        # prepare chunks
+
+        split_x = x.split(max_seq_len, dim=1)
+        split_labels = labels.split(max_seq_len, dim=1)
+        loss_weights = tuple(map(lambda t: t.shape[-1] / seq_len, split_x))
+
+        # go through each chunk and derive weighted losses
+
+        total_loss = 0.
+
+        for chunk, chunk_labels, loss_weight in zip(split_x, split_labels, loss_weights):
+            #print(chunk_labels)
+            logits_ = self.net(
+                chunk,
+                mems=mems,
+                return_mems=True,
+                **kwargs
+            )
+            #print(logits_)
+            logits = logits_#[0]
+            #mems = logits_[1]
+            loss = None
+            #print(logits.shape)
+            for i in range(self.outputs):
+                #print(logits[i])
+                loss_i = F.cross_entropy(
+                    rearrange(logits[i], 'b n c -> b c n'),
+                    chunk_labels[:, :, i].long(),
+                    ignore_index=ignore_index
+                )
+                if loss is None:
+                    loss = loss_i
+                else:
+                    loss = loss + loss_i
+
+            total_loss = total_loss + loss * loss_weight
+        return total_loss
