fixed masking issues

IOtest.py
@@ -107,14 +107,14 @@ model = MultiOXLAutoregressiveWrapper(
     net=MultiIOTransformerWrapper(
         num_tokens=[3, 3, 3],
         autoregressive=True,
-        max_seq_len=2,
+        max_seq_len=3,
         shift_mem_down=1,
         # use_abs_pos_emb=True,
         input_attn_layers=torch.nn.ModuleList([
-            AttentionLayers(dim=4, depth=1, heads=1, causal=True),
+            AttentionLayers(dim=4, depth=1, heads=1, causal=True, attn_flash=True,),
             # rotary_pos_emb=True, attn_flash=True, use_scalenorm=True, ff_glu=True),
-            AttentionLayers(dim=4, depth=1, heads=1, causal=True),
-            AttentionLayers(dim=4, depth=1, heads=1, causal=True), ]),
+            AttentionLayers(dim=4, depth=1, heads=1, causal=True, attn_flash=True,),
+            AttentionLayers(dim=4, depth=1, heads=1, causal=True, attn_flash=True,), ]),
         # rotary_pos_emb=True, attn_flash=True, use_scalenorm=True, ff_glu=True)],
         # output_attn_layers=[
         #    AttentionLayers(dim=8, depth=1, heads=2, causal=True),
@@ -139,9 +139,10 @@ print(sum(p.numel() for p in model.parameters() if p.requires_grad))
 # x = torch.Tensor(torch.randint(1, 3, (1, 10, 2))).float()
 # print(x)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-x = torch.Tensor([[[2, 1, 2], [2, 1, 0], [2, 2, 1], [1, 2, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]).long()
-# print(x.shape)
-for i in range(500):
+x = torch.Tensor([[[2, 1, 2], [2, 1, 0], [2, 2, 1], [1, 2, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+                     , [[1, 1, 2], [2, 0, 1], [2, 1, 2], [1, 2, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]).long()
+print(x.shape)
+for i in range(1000):
     loss = model(x)
     optimizer.zero_grad()
     loss.backward()

x_transformers/multi_IO/xl_autoregressive_wrapper_multiO.py
@@ -147,7 +147,7 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
         logits_total = None
         mems_total = []
         for chunk, chunk_labels, loss_weight in zip(split_x, split_labels, loss_weights):
-            mask = torch.all(chunk == self.pad_value, dim=-1)
+            mask = torch.all(chunk == self.pad_value, dim=2)
             if torch.all(mask, dim=1).all():
                 break
                 # essentially just breaking before the last chunk if the labels are all pad values
@@ -171,8 +171,9 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
             mems_total.append(mems)
 
             loss = None
-            # print(logits.shape)
             for i in range(self.outputs):
+                if torch.all(chunk_labels[:, :, i].long()==self.pad_value[i]):
+                    continue
                 loss_i = F.cross_entropy(
                     rearrange(logits[i], 'b n c -> b c n'),
                     chunk_labels[:, :, i].long(),
@@ -183,7 +184,8 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
                         loss = loss_i
                     else:
                         loss = loss + loss_i
-
+            if loss is None:
+                continue
             total_loss = total_loss + loss * loss_weight
         if not return_outputs:
             return total_loss

x_transformers/x_transformers.py
@@ -918,7 +918,7 @@ class Attention(nn.Module):
         if exists(attn_mask):
             assert 2 <= attn_mask.ndim <= 4, 'attention mask must have greater than 2 dimensions but less than or equal to 4'
             if attn_mask.ndim == 2:
-                attn_mask = rearrange(attn_mask, 'i j -> 1 1 i j')
+                attn_mask = rearrange(attn_mask, 'i j -> i 1 j 1')
             elif attn_mask.ndim == 3:
                 attn_mask = rearrange(attn_mask, 'h i j -> 1 h i j')
             masks.append(~attn_mask)