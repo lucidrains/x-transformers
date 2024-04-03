masking, index_eos_token fixes

IOtest.py
@@ -111,10 +111,10 @@ model = MultiOXLAutoregressiveWrapper(
         shift_mem_down=1,
         # use_abs_pos_emb=True,
         input_attn_layers=torch.nn.ModuleList([
-            AttentionLayers(dim=4, depth=1, heads=1, causal=True, attn_flash=True,),
+            Decoder(dim=4, depth=1, heads=1, attn_flash=True,),
             # rotary_pos_emb=True, attn_flash=True, use_scalenorm=True, ff_glu=True),
-            AttentionLayers(dim=4, depth=1, heads=1, causal=True, attn_flash=True,),
-            AttentionLayers(dim=4, depth=1, heads=1, causal=True, attn_flash=True,), ]),
+            Decoder(dim=4, depth=1, heads=1, attn_flash=True,),
+            Decoder(dim=4, depth=1, heads=1, attn_flash=True,), ]),
         # rotary_pos_emb=True, attn_flash=True, use_scalenorm=True, ff_glu=True)],
         # output_attn_layers=[
         #    AttentionLayers(dim=8, depth=1, heads=2, causal=True),
@@ -123,7 +123,7 @@ model = MultiOXLAutoregressiveWrapper(
         #    AttentionLayers(dim=8, depth=1, heads=2, causal=True)],
         # rotary_pos_emb=True, attn_flash=True, use_scalenorm=True, ff_glu=True, )],
         # l2norm_embed=True,
-        attn_layers=AttentionLayers(
+        attn_layers=Decoder(
             dim=12,
             depth=2,
             heads=4,
@@ -131,7 +131,6 @@ model = MultiOXLAutoregressiveWrapper(
             attn_flash=True,
             # use_scalenorm=True,
             # ff_glu=True,
-            causal=True
         )
     ))
 print(sum(p.numel() for p in model.parameters()))
@@ -144,11 +143,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 #print(x.shape)
 x = torch.Tensor([[[2, 1, 2], [2, 1, 0], [2, 2, 1], [1, 0, 0]]]).long()
 print(x.shape)
-print(model(x))
-x = torch.Tensor([[[2, 1, 2], [2, 1, 0], [2, 2, 1], [1, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]).long()
+print(model(x, return_outputs=True))
+x = torch.Tensor([[[2, 1, 2], [2, 1, 0], [2, 2, 1], [1, 0, 0], [2, 2, 2], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]).long()
 print(x.shape)
-print(model(x))
-
+print(model(x, return_outputs=True))
+#x = torch.Tensor([[[2, 1, 2], [2, 1, 0], [2, 2, 1]]]).long()
+#print(x.shape)
+#print(model(x, return_outputs=True))
 for i in range(1000):
     loss = model(x)
     optimizer.zero_grad()
@@ -160,11 +161,11 @@ print('generate_test: ', model.generate(prompts=torch.Tensor([[[2, 1, 2]]]).floa
 print('eos_test: ', model.generate(
     prompts=torch.Tensor([[[2, 1, 2]]]).float(),
     eos_token=torch.Tensor([[[2, 2, 1]]]).float(),
-    seq_len=3, temperature=0))
+    seq_len=100, temperature=0))
 print('eos_index_test: ', model.generate(
     prompts=torch.Tensor([[[2, 1, 2]]]).float(),
     index_eos_token={0: 1},
-    seq_len=3, temperature=0))
+    seq_len=100, temperature=0))
 
 # """
 """

x_transformers/multi_IO/IO_wrapper.py
@@ -313,7 +313,7 @@ class MultiIOTransformerWrapper(nn.Module):
                     if self.shift_mem_down and exists(cur_mem):
                         mems_l, mems_r = cur_mem[:self.shift_mem_down], cur_mem[self.shift_mem_down:]
                         cur_mem = [*mems_r, *mems_l]
-                    x_i, intermediates_pre_attn_layer = self.pre_attn_layers[i](x_i, attn_mask=mask,
+                    x_i, intermediates_pre_attn_layer = self.pre_attn_layers[i](x_i, mask=mask,
                                                                                 mems=cur_mem,
                                                                                 cache=cache_pre_attn_layers[
                                                                                     i] if cache_pre_attn_layers is not None else None,
@@ -366,7 +366,7 @@ class MultiIOTransformerWrapper(nn.Module):
             if self.shift_mem_down and exists(mems_model):
                 mems_l, mems_r = mems_model[:self.shift_mem_down], mems_model[self.shift_mem_down:]
                 mems_model = [*mems_r, *mems_l]
-            x, intermediates_model = self.attn_layers(x, attn_mask=mask, mems=mems_model, mem_masks=mem_masks,
+            x, intermediates_model = self.attn_layers(x, mask=mask, mems=mems_model, mem_masks=mem_masks,
                                                       cache=cache_model,
                                                       return_hiddens=True, seq_start_pos=seq_start_pos, **kwargs)
         else:
@@ -425,7 +425,7 @@ class MultiIOTransformerWrapper(nn.Module):
                         mems_l, mems_r = mems_cur[:self.shift_mem_down], mems_cur[self.shift_mem_down:]
                         mems_cur = [*mems_r, *mems_l]
                     if return_hiddens:
-                        post_x, inter = layer(post_x, attn_mask=mask,
+                        post_x, inter = layer(post_x, mask=mask,
                                               mems=mems_cur,
                                               mem_masks=mem_masks,
                                               cache=cache_post_attn_layers[
@@ -435,7 +435,7 @@ class MultiIOTransformerWrapper(nn.Module):
                         x_values.append(post_x)
                     else:
                         x_values.append(
-                            layer(post_x, attn_mask=mask, mems=mems_cur,
+                            layer(post_x, mask=mask, mems=mems_cur,
                                   cache=cache_post_attn_layers[i] if cache_post_attn_layers is not None else None,
                                   return_hiddens=False, seq_start_pos=seq_start_pos, **kwargs))
                     outputs.append(self.to_logits[i](x_values[i]))

x_transformers/multi_IO/xl_autoregressive_wrapper_multiO.py
@@ -77,6 +77,7 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
                 cache=cache,
                 return_mems=True,
                 return_intermediates=True,
+                mask=torch.zeros((b, x.shape[1])).bool().to(device),
                 **kwargs
             )
             logits = logits_[0]
@@ -90,7 +91,7 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
                     filtered_logits_i = filter_logits_fn(logits_i, **filter_kwargs)
                     probs_i = F.softmax(filtered_logits_i / temperature, dim=-1)
                     sample_i = torch.multinomial(probs_i, 1)
-                sample = torch.cat((sample, sample_i), dim=-1)
+                sample = torch.cat((sample, sample_i), dim=1)
             out = torch.cat((out, sample[None, :, :]), dim=1)
             if is_last_segment_tokens:
                 curr_pos = curr_segment_len
@@ -103,7 +104,10 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
 
             if exists(index_eos_token):
                 for index, eos_token in index_eos_token.items():
-                    if torch.any(torch.all(torch.eq(out[:, :, index], eos_token), dim=-1)):
+                    print(out[:, :, index])
+                    print(out[:, :, index] == eos_token)
+                    print((out[:, :, index] == eos_token).any(dim=-1))
+                    if (out[:, :, index] == eos_token).any(dim=-1).all():
                         break
         if exists(eos_token):
             # mask out everything after the eos tokens
@@ -112,7 +116,7 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
             out = torch.where(mask.unsqueeze(-1), self.pad_value, out)
         if exists(index_eos_token):
             for index, eos_token in index_eos_token.items():
-                shifted_is_eos_tokens = F.pad(torch.all(torch.eq(out[:, :, index], eos_token), dim=-1), (1, -1))
+                shifted_is_eos_tokens = F.pad(out[:, :, index] == eos_token, (1, -1))
                 mask = shifted_is_eos_tokens.float().cumsum(dim=-1) >= 1
                 out = torch.where(mask.unsqueeze(-1), self.pad_value, out)
 
@@ -161,7 +165,6 @@ class MultiOXLAutoregressiveWrapper(nn.Module):
                 mask=mask,
                 **kwargs
             )
-
             logits = logits_[0]
             if logits_total is None:
                 logits_total = logits