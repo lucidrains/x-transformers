5a3151ea6f5b599dff39a9e20c40e87aec01d30d
lucidrains <lucidrains@gmail.com>
2024-06-08 10:31:20 -0700

    add ability to turn off all normalization for each layer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'x-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '1.30.11',
+  version = '1.30.15',
   license='MIT',
   description = 'X-Transformers - Pytorch',
   author = 'Phil Wang',
