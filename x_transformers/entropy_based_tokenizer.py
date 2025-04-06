bd95a3e7c688d01456ddc592af87c6391193939c
Phil Wang <lucidrains@gmail.com>
2025-04-06 06:22:47 -0700

    take care of variable length sequences for entropy threshold tokenizer

--- x_transformers/entropy_based_tokenizer.py
+++ x_transformers/entropy_based_tokenizer.py
@@ -1,10 +1,14 @@
+from itertools import zip_longest
+
 import torch
+from torch import tensor
 import torch.nn.functional as F
 from torch.nn import Module
 from torch.nn.utils.rnn import pad_sequence
 
 from x_transformers.x_transformers import Decoder, TransformerWrapper
 
+import einx
 from einops import repeat, rearrange
 
 # helper functions
@@ -36,29 +40,56 @@ class EntropyBasedTokenizer(Module):
     def forward(
         self,
         seq,
+        lens = None, # Int['b']
         return_segmented_seq = False
     ):
         self.decoder.eval()
 
+        is_var_length = exists(lens)
         batch, seq_len, device = *seq.shape, seq.device
+
+        arange = torch.arange(seq_len, device = device)
+
+        # forward through a small trained decoder and get the entropies of the logits
 
         _, intermediates = self.decoder(seq, return_logit_entropies = True)
 
         entropies = intermediates.logit_entropies
 
-        over_thres_mask = entropies >= self.entropy_threshold
+        # get length mask for boundaries
 
-        arange = torch.arange(seq_len, device = device) + 1
-        arange = repeat(arange, 'n -> b n', b = batch)
+        mask = tensor(True, device = device)
+
+        if is_var_length:
+            mask = einx.less('n, b -> b n', arange, lens)
+
+        # the mask for tokens that were of a sufficient surprise level
+
+        over_thres_mask = (entropies >= self.entropy_threshold) & mask
+
+        # needed for selecting out indices at entropy threshold mask
+
+        arange_plus_one = arange + 1
+        arange_plus_one = repeat(arange_plus_one, 'n -> b n', b = batch)
 
         # get a tensor of Int['b num_tokens'] with the token lengths, zero padded
 
         boundaries = over_thres_mask.clone()
-        boundaries[..., -1] = True # last token is always a boundary
+
+        # set the boundary of the last token
+
+        # if `lens` not given, assume always last token
+        # but if `lens` were given, then properly set the index
+
+        if not is_var_length:
+            boundaries[..., -1] = True
+        else:
+            scatter_indices = rearrange(lens - 1, 'b -> b 1')
+            boundaries.scatter_(-1, scatter_indices, True)
 
         num_tokens = boundaries.sum(dim = -1) # number of tokens
 
-        boundaries = arange[boundaries].split(num_tokens.tolist())
+        boundaries = arange_plus_one[boundaries].split(num_tokens.tolist())
 
         # get the token lengths
 
@@ -79,12 +110,17 @@ class EntropyBasedTokenizer(Module):
 
         # segment the sequence based on the token lengths
 
+        lens = default(lens, (None,))
         segmented_seq = []
 
-        for one_seq, one_token_length in zip(seq, token_lengths):
+        for one_seq, one_len, one_token_length in zip_longest(seq, lens, token_lengths):
+
+            if exists(one_len):
+                one_seq = one_seq[:one_len]
 
             one_token_length = one_token_length[one_token_length > 0]
 
+            print(one_seq.shape, one_token_length)
             splitted_seq = one_seq.split(one_token_length.tolist())
             segmented_seq.append(splitted_seq)
 
