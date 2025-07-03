6f31a33988746b7e59951389c59a0847b03a7041
lucidrains <lucidrains@gmail.com>
2025-07-03 16:16:18 +0000

    make it run end to end

--- x_transformers/up_wrapper.py
+++ x_transformers/up_wrapper.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 from torch.nn import LSTM, Module
 
 from x_transformers.x_transformers import (
@@ -19,16 +20,23 @@ def default(v, d):
 class SyntheticDataGenerator(Module):
     def __init__(
         self,
-        dim
+        dim,
+        num_tokens,
+        max_seq_len = 512,
+        hidden_size = None
     ):
         super().__init__()
 
+        self.max_seq_len = max_seq_len
+
         self.embed = nn.Embedding(num_tokens, dim)
 
-        self.lstm = LSTM(dim, batch_first = True)
+        hidden_size = default(hidden_size, dim)
+        self.lstm = LSTM(dim, hidden_size, batch_first = True)
 
         self.to_logits = nn.Linear(dim, num_tokens, bias = False)
 
+    @torch.inference_mode()
     def generate(
         self,
         length,
@@ -36,7 +44,24 @@ class SyntheticDataGenerator(Module):
         condition = None,
         temperature = 1e-4 # he uses a near greedy temperature
     ):
-        return length
+        assert exists(seed) or exists(condition)
+        prefix = [*filter(exists, (seed, condition))]
+        seq_len = self.max_seq_len
+
+        seq = torch.cat(prefix, dim = -1)
+        hiddens = None
+
+        for _ in range(length):
+            inp = seq[:, -seq_len:]
+            logits, hiddens = self.forward(inp, hiddens)
+
+            last_logit = logits[:, -1]
+            prob = (last_logit / temperature).softmax(dim = -1)
+
+            sampled = torch.multinomial(prob, 1)
+            seq = torch.cat((seq, sampled), dim = -1)
+
+        return seq[:, -seq_len:]
 
     def forward(
         self,
@@ -57,11 +82,12 @@ class SyntheticDataGenerator(Module):
 class UniversalPretrainWrapper(Module):
     def __init__(
         self,
-        batch_size,
         model: TransformerWrapper,
-        data_generator: SyntheticDataGenerator,
+        data_generator: SyntheticDataGenerator | None = None,
         buffer_size = None,
+        batch_size = 32,
         seq_len = 512,
+        seed_length = 8
     ):
         super().__init__()
 
@@ -70,14 +96,35 @@ class UniversalPretrainWrapper(Module):
 
         assert model.attn_layers.causal
 
+        if not exists(data_generator):
+            data_generator = SyntheticDataGenerator(
+                num_tokens = model.num_tokens,
+                dim = model.attn_layers.dim
+            )
+
+        self.seq_len = seq_len
         self.data_generator = data_generator
 
-        self.register_buffer('synth_data_buffer', torch.randint((buffer_size, seq_len)))
+        self.seed_length = seed_length
+        self.batch_size = batch_size
+        buffer_size = default(buffer_size, batch_size * 20)
+        assert buffer_size > batch_size, f'data buffer size must be greater than batch size'
+
+        init_data_buffer = torch.randint(0, model.num_tokens, (buffer_size, seq_len))
+        self.register_buffer('synth_data_buffer', init_data_buffer)
+
+    @property
+    def device(self):
+        return self.synth_data_buffer.device
 
     def forward(
         self
     ):
 
-        synthetic_data = self.data_generator()
+        randperm = torch.randperm(self.batch_size, device = self.device)
+
+        seeds = self.synth_data_buffer[randperm][:self.seed_length]
+
+        synthetic_data = self.data_generator.generate(self.seq_len, seed = seeds)
 
         return self.ar_wrapped(synthetic_data)
